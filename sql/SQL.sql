9fa97c79c0b393655ae8abbcf1cde1fd902c052b
0Chord <seaweed.0chord@gmail.com>
2023-01-05 02:56:43 +0900

    Feat: 추가 SQL파일

---
+++ sql/SQL.sql
@@ -0,0 +1,30 @@
+CREATE TABLE user
+(
+    id           int          NOT NULL auto_increment primary key,
+    email        varchar(100) not null,
+    password     varchar(100) not null,
+    nickname     varchar(20)  not null,
+    phone_number varchar(20)  not null,
+    email_auth   boolean      not null default 0,
+    created_time datetime     not null
+);
+
+CREATE TABLE user_roles
+(
+    user_email varchar(100) not null,
+    roles      varchar(20)  not null
+);
+
+CREATE TABLE mail_auth
+(
+    id         int          not null auto_increment primary key,
+    user_email varchar(100) not null,
+    code       varchar(10)  not null
+);
+
+CREATE TABLE refresh_token
+(
+    refresh_token_id int          NOT NULL auto_increment primary key,
+    refresh_token    varchar(250) not null,
+    user_email       varchar(100)  not null
+);
