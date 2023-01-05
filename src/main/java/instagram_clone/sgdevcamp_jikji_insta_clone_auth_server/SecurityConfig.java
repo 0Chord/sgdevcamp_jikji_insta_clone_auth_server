edf95b9f3728b435528a0dbd943e072ecd44c757
0Chord <seaweed.0chord@gmail.com>
2023-01-05 19:17:09 +0900

    Feat: 추가 SecurityConfig
    JWT사용을 위한 기본 보안 설정

---
+++ src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/SecurityConfig.java
@@ -0,0 +1,36 @@
+package instagram_clone.sgdevcamp_jikji_insta_clone_auth_server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.jwt.JwtAuthenticationFilter;
+import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig{
+
+	private final JwtTokenProvider jwtTokenProvider;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http
+			.httpBasic().disable()
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authorizeHttpRequests()
+			.antMatchers("/").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
+}
