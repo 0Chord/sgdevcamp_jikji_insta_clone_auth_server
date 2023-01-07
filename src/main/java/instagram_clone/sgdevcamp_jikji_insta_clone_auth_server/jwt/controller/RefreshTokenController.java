557dd075b2456fddaacf6dd10d74ade303ed63c9
0Chord <seaweed.0chord@gmail.com>
2023-01-07 19:44:15 +0900

    Feat: 추가 RefreshTokenController&jwtService
    RefreshTokenController: deleteRefreshToken API 추가
    JwtService : createToken, validatedAccessToken, getRoles, removeRefreshTokenByUserEmail, checkRefreshTokenByUserEmail 메서드 추가

--- src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/jwt/controller/RefreshTokenController.java
+++ src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/jwt/controller/RefreshTokenController.java
@@ -7,14 +7,17 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.jwt.service.JwtService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
+@RequestMapping("/refreshToken")
 public class RefreshTokenController {
 
 	JwtService jwtService;
@@ -23,10 +26,10 @@ public class RefreshTokenController {
 		this.jwtService = jwtService;
 	}
 
-	@PostMapping("/refresh")
-	public ResponseEntity<?> validateRefreshToken(@RequestBody Map<String, String> body){
-		log.info("refresh Controller 실행");
-		Map<String, String> map = jwtService.validatedRefreshToken(body.get("refreshToken"));
+	@PostMapping("/validateRefreshToken")
+	public ResponseEntity<?> validateRefreshToken(@RequestBody MultiValueMap<String, String> body){
+		log.info("refreshToken Controller  Refresh Token 검증 실행");
+		Map<String, String> map = jwtService.validatedRefreshToken(body.get("refreshToken").get(0));
 
 		if(map.get("status").equals("402")){
 			log.info("Refresh Token 기한 만료");
@@ -36,4 +39,16 @@ public class RefreshTokenController {
 		log.info("Refresh Token 기한 유효");
 		return new ResponseEntity<>(map, HttpStatus.OK);
 	}
+
+	@PostMapping("/deleteRefreshToken")
+	public ResponseEntity<?> deleteRefreshToken(@RequestBody MultiValueMap<String, String> body){
+		String userEmail = body.get("userEmail").get(0);
+		if(jwtService.checkRefreshTokenByUserEmail(userEmail)){
+			jwtService.removeRefreshTokenByUserEmail(userEmail);
+			return new ResponseEntity<>("SUCCESS",HttpStatus.OK);
+		}else{
+			return new ResponseEntity<>("NotFoundUserEmail",HttpStatus.BAD_REQUEST);
+		}
+	}
+
 }
