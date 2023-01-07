5f4eabcf1868ec5005cdc77b98dc0223921109d7
0Chord <seaweed.0chord@gmail.com>
2023-01-07 19:32:00 +0900

    Feat: 추가 UserController
    login 기능 추가

---
+++ src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/user/controller/UserController.java
@@ -0,0 +1,50 @@
+package instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.controller;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.jwt.dto.TokenInfoDto;
+import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.jwt.service.JwtService;
+import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.User;
+import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@AllArgsConstructor
+@NoArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+	UserService userService;
+	JwtService jwtService;
+	BCryptPasswordEncoder encoder;
+
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestBody Map<String, String> body) {
+		String userEmail = body.get("userEmail");
+		String password = body.get("password");
+		User user = userService.findByEmail(userEmail);
+		if (user == null) {
+			//아이디가 존재하지 않습니다. 에러 추가 예정
+			return null;
+		} else if (!encoder.matches(password, user.getPassword())) {
+			//비밀번호가 일치하지 않는다. 에러 추가 예정
+			return null;
+		} else {
+			TokenInfoDto token = jwtService.createToken(userEmail, user.getRoles());
+			jwtService.login(token);
+			//클라이언트 쿠키 로직 추가
+			return null;
+		}
+	}
+}
