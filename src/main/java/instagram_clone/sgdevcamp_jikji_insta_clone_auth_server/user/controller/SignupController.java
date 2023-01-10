4f4d70327af92af31970c6b6dc22882956addc26
0Chord <seaweed.0chord@gmail.com>
2023-01-10 20:23:23 +0900

    refactor: SignupController
    SignupController : IF-Else문 제거

--- src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/user/controller/SignupController.java
+++ src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/user/controller/SignupController.java
@@ -1,7 +1,10 @@
 package instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.controller;
 
 import java.io.UnsupportedEncodingException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Objects;
 import java.util.regex.Pattern;
 
@@ -11,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -62,37 +66,41 @@ public class SignupController {
 		} else if (!Pattern.matches(passwordRegex, userForm.getPassword())) {
 			bindingResult.reject("비밀번호 형식이 잘못되었습니다.");
 			return null;
-		} else {
-			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-			String securePassword = encoder.encode(userForm.getPassword());
-			User user = User.builder().email(userForm.getEmail())
-				.password(securePassword)
-				.phone(userForm.getPhone())
-				.roles(Collections.singletonList("ROLE_USER"))
-				.nickname(userForm.getNickname()).build();
-			userService.register(user);
-			String code = mailService.sendMail(user.getEmail());
-			MailAuth mailAuth = MailAuth.builder().email(user.getEmail()).code(code).build();
-			mailAuthService.register(mailAuth);
 		}
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String securePassword = encoder.encode(userForm.getPassword());
+		User user = User.builder().email(userForm.getEmail())
+			.password(securePassword)
+			.phone(userForm.getPhone())
+			.roles(Collections.singletonList("ROLE_USER"))
+			.loginAt(LocalDate.now())
+			.updateAt(new Date())
+			.createAt(new Date())
+			.nickname(userForm.getNickname()).build();
+		userService.register(user);
+		String code = mailService.sendMail(user.getEmail());
+		MailAuth mailAuth = MailAuth.builder().email(user.getEmail()).code(code).build();
+		mailAuthService.register(mailAuth);
+
 		return null;
 	}
 
 	@PostMapping("/mailAuth")
-	public String authMail(@Validated MailAuthDto mailAuthForm){
+	public String authMail(@Validated MailAuthDto mailAuthForm) {
 		MailAuth mailAuth = mailAuthService.findByEmail(mailAuthForm.getEmail());
-		if(mailAuth == null){
+		if (mailAuth == null) {
 			log.info("이메일이 없다.");
-		}else {
-			if(!Objects.equals(mailAuth.getCode(), mailAuthForm.getCode())){
-				mailAuthService.deleteByEmail(mailAuth.getEmail());
-				log.info("코드가 다르다.");
-			}else{
-				userService.updateEmailAuth(mailAuth.getEmail());
-				mailAuthService.deleteByEmail(mailAuth.getEmail());
-				log.info("코드가 같다.");
-			}
 		}
+		assert mailAuth != null;
+		if (!Objects.equals(mailAuth.getCode(), mailAuthForm.getCode())) {
+			mailAuthService.deleteByEmail(mailAuth.getEmail());
+			log.info("코드가 다르다.");
+		}
+		userService.updateEmailAuth(mailAuth.getEmail());
+		mailAuthService.deleteByEmail(mailAuth.getEmail());
+		log.info("코드가 같다.");
+		
 		return null;
 	}
+
 }
