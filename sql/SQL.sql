bbdf8d36b799a37439a96f7bc8740233ce274122
0Chord <seaweed.0chord@gmail.com>
2023-01-10 20:21:48 +0900

    Feat: 추가 User Entity
    User Entity: created_at, updated_at, login_at 추가

--- sql/SQL.sql
+++ sql/SQL.sql
@@ -4,28 +4,30 @@ CREATE TABLE user
     email        varchar(100) not null,
     password     varchar(100) not null,
     nickname     varchar(20)  not null,
-    phone_number varchar(20)  not null,
-    email_auth   boolean      not null default 0,
-    user_profile_img varchar(250) not null,
-    created_time datetime     not null
+    phone        varchar(20)  not null,
+    status       boolean      not null default 0,
+    user_profile varchar(250) not null,
+    created_at   datetime     not null,
+    updated_at   datetime     not null,
+    login_at     datetime null
 );
 
 CREATE TABLE user_roles
 (
-    user_email varchar(100) not null,
-    roles      varchar(20)  not null
+    email varchar(100) not null,
+    roles varchar(20)  not null
 );
 
 CREATE TABLE mail_auth
 (
-    id         int          not null auto_increment primary key,
-    user_email varchar(100) not null,
-    code       varchar(10)  not null
+    id    int          not null auto_increment primary key,
+    email varchar(100) not null,
+    code  varchar(10)  not null
 );
 
 CREATE TABLE refresh_token
 (
     refresh_token_id int          NOT NULL auto_increment primary key,
     refresh_token    varchar(250) not null,
-    user_email       varchar(100)  not null
+    user_email       varchar(100) not null
 );
