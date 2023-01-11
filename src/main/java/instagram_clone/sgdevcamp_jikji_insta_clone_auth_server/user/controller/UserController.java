a785696ec9d07b1bf6171bb23329fbcaae9f9b2b
0Chord <seaweed.0chord@gmail.com>
2023-01-11 21:52:14 +0900

    Feat: 추가 UserController
    get-user-info API 추가

--- src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/user/controller/UserController.java
+++ src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/user/controller/UserController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.jwt.dto.TokenInfoDto;
 import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.jwt.service.JwtService;
 import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.User;
+import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.dao.UserInfoDao;
+import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.dto.UserInfoDto;
 import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -46,5 +48,23 @@ public class UserController {
 		userService.updateLoginAt(userEmail);
 		//클라이언트 쿠키 로직 추가
 		return null;
+	}
+
+	@PostMapping("/get-user-info")
+	public ResponseEntity<?> getUserInfo(@RequestBody Map<String, String> body){
+		String userEmail = body.get("userEmail");
+		User user = userService.findByEmail(userEmail);
+		if(user == null){
+			//유저 조회 실패시 로직
+			return null;
+		}
+		UserInfoDao userInfoDao = UserInfoDao.builder()
+			.id(user.getId())
+			.email(user.getEmail())
+			.phone(user.getPhone())
+			.nickname(user.getNickname())
+			.build();
+
+		return new ResponseEntity<>(userInfoDao,HttpStatus.OK);
 	}
 }
