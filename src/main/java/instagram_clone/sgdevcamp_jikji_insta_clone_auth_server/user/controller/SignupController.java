fe604b06315308e0fc27db7532c0c73cafc24149
0Chord <seaweed.0chord@gmail.com>
2023-01-13 20:02:48 +0900

    Feat: 추가 SignupController
    nickname 정규식 추가와 회원가입시 에러처리 추가, 메일 전송시 에러 처리 추가

--- src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/user/controller/SignupController.java
+++ src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/user/controller/SignupController.java
@@ -2,17 +2,17 @@ package instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.controller;
 
 import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Date;
-import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Pattern;
 
 import javax.mail.MessagingException;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +39,8 @@ public class SignupController {
 	private static final String phoneRegex = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$";
 	private static final String passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$";
 
+	private static final String nicknameRegex = "^(?=.*[a-zA-Z0-9가-힣])[a-zA-Z0-9가-힣]{2,16}$";
+
 	public SignupController(UserService userService, MailService mailService, MailAuthService mailAuthService) {
 		this.userService = userService;
 		this.mailService = mailService;
@@ -46,64 +48,59 @@ public class SignupController {
 	}
 
 	@PostMapping("/register")
-	public String enroll(@RequestBody Map<String, String> userForm, BindingResult bindingResult) throws
+	public ResponseEntity<?> enroll(@RequestBody UserDto userForm) throws
 		MessagingException,
 		UnsupportedEncodingException {
-		System.out.println("userForm = " + userForm);
-		User userByName = userService.findByEmail(userForm.get("email"));
-		User userByNickname = userService.findByNickname(userForm.get("nickname"));
+		User userByName = userService.findByEmail(userForm.getEmail());
+		User userByNickname = userService.findByNickname(userForm.getNickname());
 		if (userByName != null) {
-			bindingResult.reject("등록된 사용자가 있습니다.");
-			return null;
+			return new ResponseEntity<>("ExistsUserEmail", HttpStatus.OK);
 		} else if (userByNickname != null) {
-			bindingResult.reject("등록된 닉네임을 사용하는 사용자가 있습니다.");
-			return null;
-		} else if (!Pattern.matches(emailRegex, userForm.get("email"))) {
-			bindingResult.reject("이메일 형식이 잘못되었습니다.");
-			return null;
-		} else if (!Pattern.matches(phoneRegex, userForm.get("phone"))) {
-			bindingResult.reject("전화번호 형식이 잘못되었습니다.");
-			return null;
-		} else if (!Pattern.matches(passwordRegex, userForm.get("password"))) {
-			bindingResult.reject("비밀번호 형식이 잘못되었습니다.");
-			return null;
+			return new ResponseEntity<>("ExistsUserNickname", HttpStatus.OK);
+		} else if (!Pattern.matches(emailRegex, userForm.getEmail())) {
+			return new ResponseEntity<>("WrongEmailFormat", HttpStatus.OK);
+		} else if (!Pattern.matches(phoneRegex, userForm.getPhone())) {
+			return new ResponseEntity<>("WrongPhoneFormat", HttpStatus.OK);
+		} else if (!Pattern.matches(passwordRegex, userForm.getPassword())) {
+			return new ResponseEntity<>("WrongPasswordFormat", HttpStatus.OK);
+		} else if (!Pattern.matches(nicknameRegex, userForm.getNickname())) {
+			return new ResponseEntity<>("WrongNicknameFormat", HttpStatus.OK);
 		}
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		String securePassword = encoder.encode(userForm.get("password"));
-		User user = User.builder().email(userForm.get("email"))
+		String securePassword = encoder.encode(userForm.getPassword());
+		System.out.println("userForm = " + userForm.getPassword()+"    "+securePassword);
+		User user = User.builder().email(userForm.getEmail())
 			.password(securePassword)
-			.name(userForm.get("name"))
-			.phone(userForm.get("phone"))
+			.name(userForm.getName())
+			.phone(userForm.getPhone())
 			.roles(Collections.singletonList("ROLE_USER"))
-			.loginAt(LocalDate.now())
+			.loginAt(LocalDateTime.now())
+			.profile(null)
 			.status(false)
-			.updateAt(new Date())
-			.createAt(new Date())
-			.nickname(userForm.get("nickname")).build();
+			.updateAt(LocalDateTime.now())
+			.createAt(LocalDateTime.now())
+			.nickname(userForm.getNickname()).build();
 		userService.register(user);
-		String code = mailService.sendMail(user.getEmail());
+		String code = mailService.sendMail(user.getEmail(),"mail");
 		MailAuth mailAuth = MailAuth.builder().email(user.getEmail()).code(code).build();
 		mailAuthService.register(mailAuth);
 
-		return user.getEmail();
+		return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
 	}
 
 	@PostMapping("/mailAuth")
-	public String authMail(@Validated MailAuthDto mailAuthForm) {
+	public ResponseEntity<?> authMail(@RequestBody MailAuthDto mailAuthForm) {
+		System.out.println("mailAuthForm = " + mailAuthForm);
 		MailAuth mailAuth = mailAuthService.findByEmail(mailAuthForm.getEmail());
 		if (mailAuth == null) {
-			log.info("이메일이 없다.");
+			return new ResponseEntity<>("NotExistsEmail", HttpStatus.OK);
 		}
-		assert mailAuth != null;
 		if (!Objects.equals(mailAuth.getCode(), mailAuthForm.getCode())) {
-			mailAuthService.deleteByEmail(mailAuth.getEmail());
 			log.info("코드가 다르다.");
+			return new ResponseEntity<>("WrongCode", HttpStatus.OK);
 		}
 		userService.updateEmailAuth(mailAuth.getEmail());
 		mailAuthService.deleteByEmail(mailAuth.getEmail());
-		log.info("코드가 같다.");
-
-		return null;
+		return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
 	}
-
 }
