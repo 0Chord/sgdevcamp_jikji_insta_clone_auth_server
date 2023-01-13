c5506a3d5d7e3c3c652c83c7a9509f795c0cb9b6
0Chord <seaweed.0chord@gmail.com>
2023-01-13 20:01:48 +0900

    Feat: 추가 UserController
    login API에서 로그인 시도 시 에러 처리와 로그인 시 쿠키 발급 추가

--- src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/user/controller/UserController.java
+++ src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/user/controller/UserController.java
@@ -8,70 +8,73 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.jwt.dto.TokenInfoDto;
+import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.jwt.service.CookieService;
 import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.jwt.service.JwtService;
 import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.User;
 import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.dao.UserInfoDao;
+import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.dto.LoginDto;
 import instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.user.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Api(tags = "UserController")
 @RestController
-
 @RequestMapping("/user")
 public class UserController {
 
-
-
 	UserService userService;
 	JwtService jwtService;
-	BCryptPasswordEncoder encoder;
+	CookieService cookieService;
 
-	public UserController(UserService userService, JwtService jwtService) {
+
+	public UserController(UserService userService, JwtService jwtService, CookieService cookieService) {
 		this.userService = userService;
 		this.jwtService = jwtService;
+		this.cookieService = cookieService;
 	}
 
-	@Operation(summary = "로그인",description = "로그인 요청 시 JWT 클라이언트에 쿠키로 발급")
-	@ApiResponse(code=200,message = "OK")
+	@Operation(summary = "로그인", description = "로그인 요청 시 JWT 클라이언트에 쿠키로 발급")
+	@ApiResponse(code = 200, message = "OK")
 	@PostMapping("/login")
-	public String login(@RequestBody Map<String, String> body, HttpServletResponse httpServletResponse) {
-		String userEmail = body.get("email");
-		String password = body.get("password");
+	public ResponseEntity<?> login(@RequestBody LoginDto body, HttpServletResponse httpServletResponse) {
+		String userEmail = body.getEmail();
+		String password = body.getPassword();
 		User user = userService.findByEmail(userEmail);
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
 		if (user == null) {
-			return "NotFoundUser";
+			return new ResponseEntity<>("NotFoundUser",HttpStatus.OK);
 		} else if (!encoder.matches(password, user.getPassword())) {
-			//비밀번호가 일치하지 않는다. 에러 추가 예정
-			return null;
-		}else if(!user.getStatus()){
-			//이메일 인증이 안된 사용자
-			return null;
+			return new ResponseEntity<>("WrongPassword",HttpStatus.OK);
+		} else if (!user.getStatus()) {
+			return new ResponseEntity<>("NotEmailAuthUser",HttpStatus.OK);
 		}
 		TokenInfoDto token = jwtService.createToken(userEmail, user.getRoles());
 		jwtService.login(token);
 		userService.updateLoginAt(userEmail);
 		//클라이언트 쿠키 로직 추가
-		return null;
+		Cookie accessCookie = cookieService.setAccessCookie(token.getAccessToken());
+		Cookie refreshCookie = cookieService.setRefreshCookie(token.getRefreshToken());
+
+		httpServletResponse.addCookie(accessCookie);
+		httpServletResponse.addCookie(refreshCookie);
+		return new ResponseEntity<>("SUCCESS_LOGIN",HttpStatus.OK);
 	}
 
 	@PostMapping("/get-user-info")
-	public ResponseEntity<?> getUserInfo(@RequestBody Map<String, String> body){
+	public ResponseEntity<?> getUserInfo(@RequestBody Map<String, String> body) {
 		String userEmail = body.get("userEmail");
 		User user = userService.findByEmail(userEmail);
-		if(user == null){
+		if (user == null) {
 			//유저 조회 실패시 로직
 			return null;
 		}
@@ -82,6 +85,6 @@ public class UserController {
 			.nickname(user.getNickname())
 			.build();
 
-		return new ResponseEntity<>(userInfoDao,HttpStatus.OK);
+		return new ResponseEntity<>(userInfoDao, HttpStatus.OK);
 	}
 }
