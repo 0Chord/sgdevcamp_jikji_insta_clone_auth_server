8a83faa3d34eb81479ace0e92ec987f3324b8041
0Chord <seaweed.0chord@gmail.com>
2023-01-13 20:00:50 +0900

    Feat: 추가 MailService
    임시비밀번호 발급 메일 전송 로직 추가

--- src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/mail/service/MailService.java
+++ src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/mail/service/MailService.java
@@ -1,6 +1,7 @@
 package instagram_clone.sgdevcamp_jikji_insta_clone_auth_server.mail.service;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Objects;
 import java.util.Random;
 
 import javax.mail.MessagingException;
@@ -20,19 +21,19 @@ public class MailService {
 	private final SpringTemplateEngine templateEngine;
 	private String authCode;
 
-	public void createCode(){
+	public void createCode() {
 		Random random = new Random();
 		StringBuffer key = new StringBuffer();
 
-		for(int i=0;i<8;i++){
+		for (int i = 0; i < 8; i++) {
 			int index = random.nextInt(3);
 
-			switch (index){
+			switch (index) {
 				case 0:
-					key.append((char)((int)random.nextInt(26)+97));
+					key.append((char)((int)random.nextInt(26) + 97));
 					break;
 				case 1:
-					key.append((char)((int)random.nextInt(26)+65));
+					key.append((char)((int)random.nextInt(26) + 65));
 					break;
 				case 2:
 					key.append(random.nextInt(9));
@@ -42,31 +43,42 @@ public class MailService {
 		authCode = key.toString();
 	}
 
-	public MimeMessage createMailForm(String email) throws MessagingException, UnsupportedEncodingException{
+	public MimeMessage createMailForm(String email,String type) throws MessagingException, UnsupportedEncodingException {
 		createCode();
 		String setFrom = "smilestagram.clone@gmail.com";
-		String title = authCode + "is your Smilestagram code";
+
+		String title;
+		if(Objects.equals(type, "password")){
+			title = authCode + "is your Smilestagram New Password";
+		}else{
+			title = authCode + "is your Smilestagram code";
+		}
 
 		MimeMessage message = mailSender.createMimeMessage();
-		message.addRecipients(MimeMessage.RecipientType.TO,email);
+		message.addRecipients(MimeMessage.RecipientType.TO, email);
 		message.setSubject(title);
 		message.setFrom(setFrom);
-		message.setText(setContext(email,authCode),"utf-8","html");
+		message.setText(setContext(email, authCode,type), "utf-8", "html");
 
 		return message;
 	}
 
-	public String sendMail(String toEmail) throws MessagingException, UnsupportedEncodingException{
-		MimeMessage emailForm = createMailForm(toEmail);
+	public String sendMail(String toEmail,String type) throws MessagingException, UnsupportedEncodingException {
+		MimeMessage emailForm = createMailForm(toEmail,type);
 		mailSender.send(emailForm);
 
 		return authCode;
 	}
 
-	public String setContext(String email,String code){
+	public String setContext(String email, String code, String type) {
 		Context context = new Context();
-		context.setVariable("email",email);
-		context.setVariable("code",code);
-		return templateEngine.process("mail",context);
+		context.setVariable("email", email);
+		context.setVariable("code", code);
+		if (Objects.equals(type, "password")) {
+			return templateEngine.process("password", context);
+		} else {
+			return templateEngine.process("mail", context);
+		}
 	}
+
 }
