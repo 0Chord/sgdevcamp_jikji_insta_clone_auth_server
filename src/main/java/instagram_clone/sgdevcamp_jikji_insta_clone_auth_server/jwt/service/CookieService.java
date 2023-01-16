5b65b3923ebe1b343bc19ba7d0257a865d96151c
0Chord <seaweed.0chord@gmail.com>
2023-01-16 17:33:39 +0900

    Remove : 삭제 setAccessCookie
    localStorage 방식을 채택하여 삭제

--- src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/jwt/service/CookieService.java
+++ src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/jwt/service/CookieService.java
@@ -7,14 +7,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class CookieService {
 
-	public Cookie setAccessCookie(String accessToken){
-		Cookie cookie = new Cookie("accessToken",accessToken);
-		cookie.setMaxAge(2*60*60);
-		cookie.setPath("/");
-		cookie.setHttpOnly(true);
-		return cookie;
-	}
-
 	public Cookie setRefreshCookie(String refreshToken){
 		Cookie cookie = new Cookie("refreshToken",refreshToken);
 		cookie.setHttpOnly(true);
