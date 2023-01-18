0935a834e9583cd656b2e0cfd143d3f60ca93bf0
0Chord <seaweed.0chord@gmail.com>
2023-01-18 20:47:07 +0900

    Remove : 삭제 CookieService
    refreshToken Localstorage를 통한 인증으로 전환때문에 CookieService 삭제

--- src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/jwt/service/JwtService.java
+++ src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/jwt/service/JwtService.java
@@ -26,61 +26,61 @@ public class JwtService {
 		this.refreshTokenRepository = refreshTokenRepository;
 	}
 
-	public TokenInfoDto createToken(String userEmail, List<String> roles){
-		return jwtTokenProvider.createToken(userEmail,roles);
+	public TokenInfoDto createToken(String userEmail, List<String> roles) {
+		return jwtTokenProvider.createToken(userEmail, roles);
 	}
 
 	@Transactional
-	public void login(TokenInfoDto tokenDto){
+	public void login(TokenInfoDto tokenDto) {
 		RefreshToken refreshToken = RefreshToken.builder().userEmail(tokenDto.getUserEmail()).refreshToken(
 			tokenDto.getRefreshToken()).build();
-		if(refreshTokenRepository.existsByUserEmail(refreshToken.getUserEmail())){
+		if (refreshTokenRepository.existsByUserEmail(refreshToken.getUserEmail())) {
 			log.info("refresh token 검사");
 			refreshTokenRepository.deleteByUserEmail(refreshToken.getUserEmail());
 		}
 		refreshTokenRepository.save(refreshToken);
 	}
 
-	public Optional<RefreshToken> getRefreshToken(String refreshToken){
+	public Optional<RefreshToken> getRefreshToken(String refreshToken) {
 		return refreshTokenRepository.findByRefreshToken(refreshToken);
 	}
 
-	public Map<String, String> validatedRefreshToken(String refreshToken){
+	public Map<String, String> validatedRefreshToken(String refreshToken) {
 		RefreshToken refreshToken1 = getRefreshToken(refreshToken).get();
 		String createdAccessToken = jwtTokenProvider.validateRefreshToken(refreshToken1);
 
 		return createdRefreshJson(createdAccessToken);
 	}
 
-	public Boolean validatedAccessToken(String accessToken){
+	public Boolean validatedAccessToken(String accessToken) {
 		return jwtTokenProvider.validateAccessToken(accessToken);
 	}
 
-	public String getRoles(String accessToken){
+	public String getRoles(String accessToken) {
 		return jwtTokenProvider.getRoles(accessToken);
 	}
 
-	public Map<String, String> createdRefreshJson(String createdAccessToken){
+	public Map<String, String> createdRefreshJson(String createdAccessToken) {
 		Map<String, String> map = new HashMap<>();
-		if(createdAccessToken == null){
-			map.put("errorType","Forbidden");
-			map.put("status","402");
-			map.put("message","RefreshToken이 만료되었습니다. 로그인을 다시 해주시길 바랍니다.");
+		if (createdAccessToken == null) {
+			map.put("errorType", "Forbidden");
+			map.put("status", "402");
+			map.put("message", "RefreshToken이 만료되었습니다. 로그인을 다시 해주시길 바랍니다.");
 
 			return map;
 		}
-		map.put("status","200");
-		map.put("message","Refresh Token을 통한 AccessToken 생성이 완료되었습니다,");
-		map.put("accessToken",createdAccessToken);
+		map.put("status", "200");
+		map.put("message", "Refresh Token을 통한 AccessToken 생성이 완료되었습니다,");
+		map.put("accessToken", createdAccessToken);
 
 		return map;
 	}
 
-	public void removeRefreshTokenByUserEmail(String userEmail){
+	public void removeRefreshTokenByUserEmail(String userEmail) {
 		refreshTokenRepository.deleteByUserEmail(userEmail);
 	}
 
-	public Boolean checkRefreshTokenByUserEmail(String userEmail){
-		return  refreshTokenRepository.existsByUserEmail(userEmail);
+	public Boolean checkRefreshTokenByUserEmail(String userEmail) {
+		return refreshTokenRepository.existsByUserEmail(userEmail);
 	}
 }
