d34fc9c11d28fcf45da0bf197491e8c470ec5344
0Chord <seaweed.0chord@gmail.com>
2023-01-19 20:41:37 +0900

    Feat: 추가 SecurityConfig
    BCryptPasswordEncorder 스프링 빈 등록

--- src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/SecurityConfig.java
+++ src/main/java/instagram_clone/sgdevcamp_jikji_insta_clone_auth_server/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -39,5 +40,10 @@ public class SecurityConfig{
 			.and()
 			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 		return http.build();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
 	}
 }
